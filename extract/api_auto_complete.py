88efe65c98ace4474412ef5c8d7b0ba6d74566dd
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2023-05-11 15:41:04 +1000

    Update query naming

--- extract/api_auto_complete.py
+++ extract/api_auto_complete.py
@@ -6,10 +6,13 @@ from api_variables import api_host, headers
 url = 'https://' + api_host + "/auto-complete"
 
 query = input('Enter a search query: ')
-query_object = {"query": query}
+queryParams = {"query": query}
 
-response = requests.get(url, headers=headers, params=query_object)
+response = requests.get(url, headers=headers, params=queryParams)
 
-print(response.json())
-with open("auto_complete_out.json", "w") as output:
-    json.dump(response.json(), output)
+if response.status_code == 200:
+    print(response.json())
+    with open("auto_complete_out.json", "w") as output:
+        json.dump(response.json(), output)
+else:
+    print(f"Error: {response.status_code}")
