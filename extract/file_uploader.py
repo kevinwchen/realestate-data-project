2940eeb3698355b325ff6072e9ad5f8b435c254d
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2023-05-13 21:59:42 +1000

    Renaming files and update readme

--- extract/file_uploader.py
+++ extract/file_uploader.py
@@ -7,28 +7,24 @@ load_dotenv()
 
 aws_key = os.getenv('AWS-ACCESS-KEY')
 aws_secret = os.getenv('AWS-SECRET-KEY')
+aws_bucket_name = os.getenv('AWS-BUCKET-NAME')
 
 def main():
-    # Create a client with the MinIO server playground, its access key
-    # and secret key.
+    # Create a client with the MinIO server playground, its access key and secret key.
     client = Minio(
-        "s3.amazonaws.com",
-        access_key=aws_key,
-        secret_key=aws_secret,
+        "s3.amazonaws.com", access_key=aws_key, secret_key=aws_secret,
     )
-    
-    bucket_name = "kevinwchen-bucket-real-estate"
 
-    # Make 'real-estate' bucket if not exist.
-    found = client.bucket_exists(bucket_name)
+    # Make bucket if not existing
+    found = client.bucket_exists(aws_bucket_name)
     if not found:
-        client.make_bucket(bucket_name)
+        client.make_bucket(aws_bucket_name)
     else:
-        print(f"Bucket '{bucket_name}' already exists")
+        print(f"Bucket '{aws_bucket_name}' already exists")
 
     # Upload data
-    client.fput_object(bucket_name, "properties.json", os.path.join(os.path.dirname(__file__), "output", "properties.json"))
-    print(f"Data uploaded to bucket {bucket_name}")
+    client.fput_object(aws_bucket_name, "properties.json", os.path.join(os.path.dirname(__file__), "outputs", "properties.json"))
+    print(f"Data uploaded to bucket {aws_bucket_name}")
 
 if __name__ == "__main__":
     try:
--- minio.md
+++
@@ -1,9 +0,0 @@
-# MinIO
-
-## Windows
-
-Start minio server in download directory using:
-
-```
-.\minio.exe server c:\minio --console-address :9090
-```
