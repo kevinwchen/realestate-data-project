9b4591f7b3913daab047e942c494a85bcf6f4d20
kevin <47770053+kevinwchen@users.noreply.github.com>
2023-06-09 16:50:34 +1000

    Add reading of JSON from S3

--- transform/clean.py
+++ transform/clean.py
@@ -10,5 +10,8 @@ s3 = boto3.resource("s3")
 my_bucket = s3.Bucket(bucket_name)
 
 for object_summary in my_bucket.objects.filter(Prefix="raw/"):
-    print(object_summary.key)
-
+    obj = s3.Object(bucket_name, object_summary.key)
+    file_content = obj.get()['Body'].read().decode('utf-8')
+    json_content = json.loads(file_content)
+    for content in json_content:
+        print(content["price"]["display"])
