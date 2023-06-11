22473a3dd3785bd2ee2ce34a21955f30a3b0af3b
kevin <47770053+kevinwchen@users.noreply.github.com>
2023-06-11 10:54:24 +1000

    Add basic number filter

--- transform/clean.py
+++ transform/clean.py
@@ -5,13 +5,25 @@ from aws_access import bucket_name
 
 # Configure AWS CLI before running this
 
-s3 = boto3.resource("s3")
+def main():
+    s3 = boto3.resource("s3")
 
-my_bucket = s3.Bucket(bucket_name)
+    my_bucket = s3.Bucket(bucket_name)
 
-for object_summary in my_bucket.objects.filter(Prefix="raw/"):
-    obj = s3.Object(bucket_name, object_summary.key)
-    file_content = obj.get()['Body'].read().decode('utf-8')
-    json_content = json.loads(file_content)
-    for content in json_content:
-        print(content["price"]["display"])
+    for object_summary in my_bucket.objects.filter(Prefix="raw/"):
+        obj = s3.Object(bucket_name, object_summary.key)
+        file_content = obj.get()['Body'].read().decode('utf-8')
+        json_content = json.loads(file_content)
+        for content in json_content:
+            displayPrice = content["price"]["display"]
+            if hasNum(displayPrice):
+                print(displayPrice)
+
+def hasNum(inputString):
+    return any(char.isdigit() for char in inputString)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except all:
+        print("Error occurred.")
